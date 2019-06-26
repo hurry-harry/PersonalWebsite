d6f32ad57da57a1224cd4d8d08cb2137f32771c8
Harold Francisco <hfrancisco@you-source.com>
2019-06-26 10:15:20 +0800

    Revert "Test deploy"
    This reverts commit 4dcc2f3d41c72b2ac04f441619f117f43eca4927.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { AboutComponent } from './home/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/about/about.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  currJobTitle = 'Junior Developer';
+  currCompany: {[key: string]: string} = {
+    name: 'YouSource Inc',
+    link: 'https://www.you-source.com/'
+  };
+  currLocation: {[key: string]: string} = {
+    city: 'Makati City',
+    country: 'Philippines',
+    countryISO: 'PHL'
+  };
+
+  isToggledTechStackAccordion: boolean;
+  isToggledWorkExpAccordion: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.isToggledTechStackAccordion = false;
+    this.isToggledWorkExpAccordion = false;
+  }
+
+  toggleTechStackAccordion(): void {
+    this.isToggledTechStackAccordion = !this.isToggledTechStackAccordion;
+  }
+
+  toggleWorkExpAccordion(): void {
+    this.isToggledWorkExpAccordion = !this.isToggledWorkExpAccordion;
+  }
+}
